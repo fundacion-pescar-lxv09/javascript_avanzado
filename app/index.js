26af3e0b80da4815358a5d80d866119f888c0beb
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-06-05 12:12:14 -0300

    logica para alternar clases de un elemento

--- app/index.js
+++ app/index.js
@@ -1,5 +1,15 @@
 /* Declaraciones */
+const d = document;
+const navButton = d.querySelector("nav .nav__button");
+const navMenu = d.querySelector("nav ul");
+
+const mapButton = d.querySelector("footer .map__button");
+const mapFrame = d.querySelector("iframe.map");
 
 /* Funciones */
-
+function toggle(element, className="active"){
+    element.classList.toggle(className);
+}
 /* Eventos */
+navButton.onclick = () => toggle(navMenu)
+mapButton.onclick = () => toggle(mapFrame)
