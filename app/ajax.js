93b8c6a00f08c9174a11ba1be726af240e25c052
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-06-10 11:04:22 -0300

    peticion asincrona con xhr

--- app/ajax.js
+++ app/ajax.js
@@ -1,4 +1,22 @@
 // Esperar a que se carguen todos los nodos del DOM
 d.addEventListener('DOMContentLoaded', function() {
-    
+
+    // Trabajando con AJAX
+    function ajax(url, data){
+        const { method } = data;
+        const xhr = new XMLHttpRequest();
+
+        xhr.addEventListener('load', () => {
+            console.log("cargado")
+        })
+        xhr.addEventListener('readystatechange', (e) => {
+            if(e.target.readyState === 4 && e.target.status === 200){
+                d.querySelector('main').innerHTML = e.target.response
+            }
+        })
+        xhr.open(method, url);
+        xhr.send()
+    }
+
+    ajax('/assets/content/AJAX.txt',{method: "GET"})
 })
