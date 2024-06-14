906267255ce3eb199b9967e055a673856ec7e578
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-06-14 10:31:20 -0300

    vista de publicaciones

--- app/views/post.js
+++ app/views/post.js
@@ -1,3 +1,13 @@
-export const posts = () => `
-
+export const posts = ({id,userId,title,body,callback}) => `
+    <article id="post_${id}" class="card p-0">
+        <h2 class="card-header text-bg-dark fs-3">${title}</h2>
+        <p class="card-body">${body}</p>
+        <footer class="card-footer">
+            <p>Publicado por el usuario ${userId}</p>
+            <section id="comments_${id}">
+                <h3 class="fs-5">Comentarios</h3>
+                ${callback && callback(id) || '<p>sin comentarios<p>'}
+            </section>
+        </footer>
+    </article>
 `
