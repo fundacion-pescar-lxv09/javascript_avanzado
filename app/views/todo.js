7e9c0a3f0279b8605b33314719a4b446b3985634
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-06-14 10:01:08 -0300

    vista de tareas

--- app/views/todo.js
+++ app/views/todo.js
@@ -1,3 +1,6 @@
-export const todos = () => `
-
+export const todos = ({id, title, completed}) => `
+    <div id="todo_${id}" class="col-md-8 col-lg-6 col-xxl-4 d-flex justify-content-between align-items-center border-bottom p-2">
+        <p class="h6">${title}</p>
+        <button class="btn fa fa-${completed?'check-circle btn-outline-success':'times-circle btn-outline-danger'}"></button>
+    </div>
 `
