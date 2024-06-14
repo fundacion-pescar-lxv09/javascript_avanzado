5576ef55dbb6a7053347aacb44a11309baab2d23
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-06-14 11:14:10 -0300

    barrade navegacion con links

--- app/views/user.js
+++ app/views/user.js
@@ -1,31 +1,31 @@
 export const users = ({id, name, username, email, phone, address, website, company}) => `
-<article id="user_${id}" class="card p-0 col-md-5 col-lg-5">
-    <header class="card-header">
-        <h2>${username}</h2>
-        <p>${name}</p>
-    </header>
-    <div class="card-body">
-        <h3 class="h5">Datos personales</h3>
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">${address.street}</li>
-            <li class="list-group-item">${address.suite}</li>
-            <li class="list-group-item">${address.city}</li>
-        </ul>
-        <h3 class="h5">Datos Laborales</h3>
-        
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">${company.name}</li>
-            <li class="list-group-item">${company.catchPhrase}</li>
-            <li class="list-group-item">${company.bs}</li>
-        </ul>
-    </div>
-    <footer class="card-footer">
-        <h3 class="h5">Contactame</h3>
-        <address>
-            Llamando al ${phone} <br>
-            Enviando un correo a <a href="mailto:${email}">${email}</a> <br>
-            visitando el sitio <a href="${website}" target="_blank">${website}</a>
-        </address>
-    </footer>
-<article>
+    <article id="user_${id}" class="card p-0 col-md-5 col-lg-5">
+        <header class="card-header">
+            <h2>${username}</h2>
+            <p>${name}</p>
+        </header>
+        <div class="card-body">
+            <h3 class="h5">Datos personales</h3>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">${address.street}</li>
+                <li class="list-group-item">${address.suite}</li>
+                <li class="list-group-item">${address.city}</li>
+            </ul>
+            <h3 class="h5">Datos Laborales</h3>
+            
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">${company.name}</li>
+                <li class="list-group-item">${company.catchPhrase}</li>
+                <li class="list-group-item">${company.bs}</li>
+            </ul>
+        </div>
+        <footer class="card-footer">
+            <h3 class="h5">Contactame</h3>
+            <address>
+                Llamando al ${phone} <br>
+                Enviando un correo a <a href="mailto:${email}">${email}</a> <br>
+                visitando el sitio <a href="${website}" target="_blank">${website}</a>
+            </address>
+        </footer>
+    <article>
 `
