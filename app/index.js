5a106a22a6175b63968f05d4caadda89246e5019
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-06-24 11:03:59 -0300

    definir clase para piezas y actualizar renderizado

--- app/index.js
+++ app/index.js
@@ -1,10 +1,54 @@
 const d = document;
 const root = d.getElementById('root');
-const img = "assets/checker.svg";
+const checker = "assets/checker.svg";
 
 const checkCoord = (row, col) => 
     (row % 2 === 0 && col % 2 === 0) || 
     (row % 2 === 1 && col % 2 === 1)
+
+class Piece {
+    isKing = false;
+    constructor(color, row, col){
+        this.color = color;
+        this.row = row
+        this.col = col
+    }
+    makeKing(){
+        this.isKing = true;
+    }
+    isValidMove(x,y){
+        return (
+        (x === this.row + 1 || x === this.row - 1) && 
+        (y === this.col + 1 || y === this.col - 1) &&
+        (x >= 0 && x < 8) && 
+        (y >= 0 && y < 8)
+    )}
+    movePiece(nextRow,nextCol){
+        if (this.isValidMove(nextRow,nextCol)) {
+            this.row = nextRow
+            this.col = nextCol
+        }
+        else alert("Movimiento no permitido");
+    }
+    render(type = "string", element = ''){
+        return type === "string" ? this.template() : this.create(element)
+    }
+    create(element){
+        const img = document.createElement('img');
+        Object.assign(img, {
+            className: this.color,
+            src: checker,
+            "data-row": this.row,
+            "data-col": this.col,
+            alt: "pieza jugador " + this.color
+        })
+        element.appendChild(img);
+    }
+    template(){
+        return `<img class="${this.color}" src="${checker}" alt="pieza jugador ${this.color}">`
+    }
+
+}
 
 class Board {
     board = [];
@@ -18,11 +62,11 @@ class Board {
             for(let c = 0; c < this.cols; c++){
                 switch(true){
                 case r < 3: {
-                    checkCoord(r,c) ? this.board[r].push("black") : this.board[r].push(null)
+                    checkCoord(r,c) ? this.board[r].push(new Piece("black",r,c)) : this.board[r].push(null)
                 break;
                 }
                 case r > 4: {
-                    checkCoord(r,c) ? this.board[r].push("white") : this.board[r].push(null)
+                    checkCoord(r,c) ? this.board[r].push(new Piece("red",r,c)) : this.board[r].push(null)
                 break;
                 }
                 default: this.board[r].push(null);
@@ -42,7 +86,7 @@ class Board {
             }
             cell.setAttribute("data-row",rowIndex);
             cell.setAttribute("data-col",cellIndex);
-            cell.innerHTML=c
+            cell.innerHTML= c?.render() ?? ''
             board.appendChild(cell);
         }))
         element.appendChild(board)
