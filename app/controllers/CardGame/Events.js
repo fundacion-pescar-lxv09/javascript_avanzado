9f3ad74d5f9c8acd5065d8babaf355bd0828b787
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-07-03 13:25:18 -0300

    funciones de eventos controladores y renderizado

--- app/controllers/CardGame/Events.js
+++ app/controllers/CardGame/Events.js
@@ -1,28 +1,49 @@
-function Events(deck, player){
-    const cards = document.querySelectorAll('.suit');
+export default function Events(deck, player, container){
     const buttons = ["start", "draw","bet", "split", "surrender"]
     buttons.forEach(b => {
-        const btn = document.getElementById(b);
-        btn.addEventListener('click', eval("()=> b()"))
+        document.addEventListener('click', ({target})=>{
+            if(target.id === b ) eval(`${b}(target)`)
+        })
     })
-    function start(){
+    function start(target){
         for(let i = 0; i < 2; i++) getCard()
+        isWinner() && celebrate()
+        target.setAttribute("disabled", true);
     }
     function draw(){
         if(next()){
             getCard()
-            player.score === 21 && alert(player.win())
-            check() && alert(player.loose());
+            isWinner() && celebrate()
+            check() && cry()
         }
     }
     function bet(){
-
+        hasMoney(10) && player.bet(10)
+        renderPlayer(player)
     }
     function split(){
-
+        hasMoney(player.ammount) && player.split();
+        renderPlayer(player)
     }
     function surrender(){
-
+        cry()
+    }
+    function isWinner(){
+        return player.score === 21;
+    }
+    function celebrate(){
+        showMessage(player.win())
+    }
+    function cry(){
+        showMessage(player.loose())
+    }
+    function showMessage(msg){
+        setTimeout(() => { alert(msg);
+            container.innerHTML = renderGame(deck,player)
+        }, 500)
+    }
+    function hasMoney(ammount){
+        return player.money >= ammount
     }
     function next(){
         return player.score < 21
@@ -30,12 +51,35 @@ function Events(deck, player){
     function check(){
         return player.score > 21
     }
-    function getCard(space){
+    function getCard(){
+        const cards = document.querySelectorAll('.box .suit');
         const space = document.querySelector('.card-space:not(.occupied)')
-        const current = cards[cards.length - 1]
+        const current = Array.from(cards).pop()
         space.appendChild(current);
         space.classList.add('occupied');
-        Array.from(cards).pop()
-        player.score+=parseInt(current.dataset.value);
+        player.score+=calcScore(parseInt(current.dataset.value));
     }
+    function calcScore(n){
+        return n === 11 ? (player.score >= 11 ? 1 : n) : n
+    }
+}
+export function renderGame(deck, player){
+    deck.shuffle();
+    player.score = 0;
+    return `
+        ${player.render()}
+        <div class="box">${ deck.render() }</div>
+        ${ deck.createMat() }
+        <div class="controls">
+            <button id="start">Empezar Nuevo Juego</button>
+            <button id="draw">Pedir</button>
+            <button id="bet">Apostar</button>
+            <button id="split">Doblar Apuesta</button>
+            <button id="surrender">Rendirse</button>
+        </div>
+    `
+}
+export function renderPlayer(player){
+    const gameScore = document.querySelector('.player')
+    gameScore.outerHTML = player.render()
 }
--- app/controllers/CardGame/index.js
+++ app/controllers/CardGame/index.js
@@ -1,4 +1,5 @@
 import Deck from "./Deck.js"
+import Events,{renderGame} from "./Events.js";
 import Player from "./Player.js"
 export const CardGame = () => {
     const container = document.createElement('div');
@@ -8,18 +9,8 @@ export const CardGame = () => {
     deck.shuffle();
     Object.assign(container, {
         className: "container mx-auto p-3",
-        innerHTML: `
-            ${player.render()}
-            <div class="box">${ deck.render() }</div>
-            ${ deck.createMat() }
-            <div class="controls">
-                <button id="start">Empezar Nuevo Juego</button>
-                <button id="draw">Pedir</button>
-                <button id="bet">Apostar</button>
-                <button id="split">Doblar Apuesta</button>
-                <button id="surrender">Rendirse</button>
-            </div>
-        `
+        innerHTML: renderGame(deck,player)
     })
+    Events(deck, player, container);
     return container;
 }
